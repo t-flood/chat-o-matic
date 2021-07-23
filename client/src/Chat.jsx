636b4798e56d5cd5855d486c5a70c144721a093c
Tim Flood <tflood9@gmail.com>
2021-07-22 20:26:14 -0400

    created chat component

---
+++ client/src/Chat.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+const Chat = () => {
+  return (
+    <div>I'm a chat window</div>
+  )
+}
+
+export default () => (
+  <ApolloProvider client={client}>
+    <Chat />
+  </ApolloProvider>
+  )
